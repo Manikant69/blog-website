1b01805c82367e10e91b0e4674b65f9f20b9ad13
Manikant Singh <manikant17082001@gmail.com>
2024-10-19 00:22:03 +0530

    adding cors module

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const blogroute = require('./routes/blogroute');
 const app = express();
 
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
